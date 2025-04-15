a0f5bbd12494c2cbd073961c58080abc17364c5a
jberry03 <berryjil000@gmail.com>
2025-04-14 19:59:53 -0700

    updated

--- server.py
+++ server.py
@@ -220,5 +220,6 @@ def get_data():
 
 # Running app
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
+    # port = int(os.environ.get("PORT", 10000))
+    # app.run(host='0.0.0.0', port=port)
